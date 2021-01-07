6475d296f4129df7816c9a31853a0c7c4d3f72e7
Abdul Hadi Butt <hadibutt476@gmail.com>
2021-01-07 20:17:53 +0500

    Update index.php

--- index.php
+++ index.php
@@ -217,6 +217,11 @@ session_start();
                             <img src="Kaygo images\Flags icon\USA_-512.png" style="height:20px;width:20px;"></img>
                             <a href="" style="color:black;">English</a>
                         </div>
+                    
+                    <div>
+                            <img src="Kaygo images\Flags icon\USA_-512.png" style="height:20px;width:20px;"></img>
+                            <a href="" style="color:black;">Latin</a>
+                        </div>
 
                         <div>
                             <img src="Kaygo images\Flags icon\China-512.png" style="height:20px;width:20px;"></img>
